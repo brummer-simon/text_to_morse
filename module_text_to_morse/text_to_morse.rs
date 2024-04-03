626299226295ea8c73b8f4165ddcd05262c9c6b0
simon <simon.brummer@posteo.de>
2024-04-03 06:21:57 +0200

    Copied example of kernel module

---
+++ module_text_to_morse/text_to_morse.rs
@@ -0,0 +1,25 @@
+use kernel::prelude::*;
+
+module! {
+    type: TextToMorse,
+    name: "TextToMorse",
+    author: "Simon Brummer <simon.brummer@posteo.de>",
+    description: "Module converting text to morse code",
+    license: "GPL",
+}
+
+struct TextToMorse {
+}
+
+impl kernel::Module for TextToMorse {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        pr_info!("TextToMorse init\n");
+        Ok(TextToMorse {})
+    }
+}
+
+impl Drop for TextToMorse {
+    fn drop(&mut self) {
+        pr_info!("TextToMorse exit\n");
+    }
+}
