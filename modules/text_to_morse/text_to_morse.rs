7e9c155c8bb2d02e9cc125cbf89fbeab86ac5312
simon <simon.brummer@posteo.de>
2024-04-18 20:06:20 +0200

    WIP

--- modules/text_to_morse/text_to_morse.rs
+++ modules/text_to_morse/text_to_morse.rs
@@ -4,7 +4,7 @@
 // TODO: Pass blink frequency as kernel parameter
 // TODO: Figure timer stuff out
 // TODO: Document important stuff
-use alloc::vec::Vec;
+use alloc::{string::String, vec::Vec};
 use kernel::chrdev;
 use kernel::file::{
     self,
@@ -15,8 +15,9 @@ use kernel::sync::{self, smutex::Mutex, Arc};
 use kernel::{prelude::*, ForeignOwnable};
 
 // Constants
-const MAX_DEVICES: usize = 1;
 const MINOR_IDS_BEGIN: u16 = 0;
+const MAX_DEVICES: usize = 1;
+const BUFFER_SIZE: usize = 1024;
 
 // Kernel module registration.
 module! {
@@ -76,14 +77,19 @@ impl Drop for Module {
 struct DeviceState {
     has_readers: bool,
     has_writers: bool,
+    buffer: String,
 }
 
 impl DeviceState {
-    fn new() -> Mutex<Self> {
-        Mutex::new(Self {
+    fn try_new() -> Result<Mutex<Self>> {
+        let mut buffer = String::new();
+        buffer.try_reserve_exact(BUFFER_SIZE)?;
+
+        Ok(Mutex::new(Self {
             has_readers: false,
             has_writers: false,
-        })
+            buffer: buffer,
+        }))
     }
 }
 
@@ -93,15 +99,10 @@ struct Device {
 }
 
 impl Device {
-    fn new(id: usize) -> Self {
-        Self {
-            id,
-            state: DeviceState::new(),
-        }
-    }
-
     fn try_new(id: usize) -> Result<Arc<Self>> {
-        Arc::try_new(Device::new(id))
+        let state = DeviceState::try_new()?;
+        let device = Device { id, state };
+        Arc::try_new(device)
     }
 
     fn get_or_allocate_device(id: usize) -> Result<Arc<Device>> {
@@ -219,22 +220,104 @@ impl file::Operations for Device {
     }
 
     fn write(
-        _data: <Self::Data as ForeignOwnable>::Borrowed<'_>,
+        device: <Self::Data as ForeignOwnable>::Borrowed<'_>,
         _file: &file::File,
-        _buffer: &mut impl IoBufferReader,
+        buffer: &mut impl IoBufferReader,
         _offset: u64,
     ) -> Result<usize> {
+        // Write: Implements "backend" of syscall write.
         pr_debug!("Called write(...)\n");
+
+        // Determine available space in our buffer. If no space is available,
+        // return EAGAIN to indicate that a later attempt might succeed
+        let mut state = device.state.lock();
+        let available_bytes = BUFFER_SIZE - state.buffer.len();
+
+        if available_bytes == 0 {
+            return Err(EAGAIN)
+        }
+
+
+        // Parse buffer char by char, verify utf-8 encoding and append each character
+        // in our buffer until it is unable to store anymore data.
+
+        let mut bytes = Vec::new();
+        let read_bytes = try_read_codepoint(buffer, &mut bytes)?;
+        let char = String::from_utf8(bytes)?;
+        state.buffer.try_push(char)?;
+
+        // TODO: Update read and available bytes
         Err(EOPNOTSUPP)
     }
 
     fn read(
-        _data: <Self::Data as ForeignOwnable>::Borrowed<'_>,
+        _device: <Self::Data as ForeignOwnable>::Borrowed<'_>,
         _file: &file::File,
         _buffer: &mut impl IoBufferWriter,
         _offset: u64,
     ) -> Result<usize> {
+        // TODO: Implement write without translation
         pr_debug!("Called read(...)\n");
         Err(EOPNOTSUPP)
     }
+}
+
+fn try_read_codepoint(buffer: &mut impl IoBufferReader, bytes: &mut Vec<u8>) -> Result<usize> {
+    // Assumption: The buffer contains a UTF-8 encoded string. With UTF-8 being a variable
+    // length encoding, the buffer could hold parts of a character. Therefore we can't just
+    // read the entire buffer into a string (verifies encoding on construction).
+    // Instead this function performs some bit fiddling to determine the number of bytes
+    // of the next character and stores these bytes into the given vector.
+    // If something unexpected is encountered, return an Error.
+
+    // Bitfiddling constants
+    const MASK_1BYTE: u8 = 0b10000000;
+    const BITS_1BYTE: u8 = 0b00000000;
+    const MASK_2BYTE: u8 = 0b11100000;
+    const BITS_2BYTE: u8 = 0b11000000;
+    const MASK_3BYTE: u8 = 0b11110000;
+    const BITS_3BYTE: u8 = 0b11100000;
+    const MASK_4BYTE: u8 = 0b11111000;
+    const BITS_4BYTE: u8 = 0b11110000;
+    const MASK_NEXT_BYTE: u8 = 0b11000000;
+    const BITS_NEXT_BYTE: u8 = 0b10000000;
+
+    // Read first byte to determine the number of bytes of this character.
+    let byte: u8 = buffer.read()?;
+    let remaining_bytes = {
+        if (byte & MASK_1BYTE) == BITS_1BYTE {
+            Ok(0)
+        }
+        else if (byte & MASK_2BYTE) == BITS_2BYTE {
+            Ok(1)
+        }
+        else if (byte & MASK_3BYTE) == BITS_3BYTE {
+            Ok(2)
+        }
+        else if (byte & MASK_4BYTE) == BITS_4BYTE {
+            Ok(3)
+        }
+        else {
+            // Buffer contents are not a utf-8 encoded string.
+            // Return EILSEQ (Illegal byte sequence)
+            Err(EILSEQ)
+        }
+    }?;
+
+    // Read additional bytes and verify them
+    bytes.clear();
+    bytes.try_push(byte)?;
+
+    for _ in 0..remaining_bytes {
+        let byte: u8 = buffer.read()?;
+
+        if (byte & MASK_NEXT_BYTE) == BITS_NEXT_BYTE {
+            bytes.try_push(byte)?;
+        } else {
+            // Buffer contents are not a utf-8 encoded string.
+            // Return EILSEQ (Illegal byte sequence)
+            return Err(EILSEQ);
+        }
+    }
+    Ok(bytes.len())
 }
